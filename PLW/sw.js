30e440db0a53eee0aa7f17aeebc423598f7c7ea6
dereka1662 <154001670+dereka1662@users.noreply.github.com>
2023-12-23 18:15:49 +0800

    V0

---
+++ PLW/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('static').then((cache) => cache.addAll([
+      "./","./logo192.png"
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
+
